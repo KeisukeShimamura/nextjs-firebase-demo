f2fc1287c860d25fd5c652ca4062fc57eab54d4c
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-12-23 23:54:12 +0900

    検索結果画面にアバター画像を表示

--- pages/search.tsx
+++ pages/search.tsx
@@ -31,10 +31,21 @@ const Hit: HitsProps<Post>["hitComponent"] = ({ hit }) => {
       <h2 className="line-clamp-2">
         <Link href={`/posts/${hit.id}`}>{hit.title}</Link>
       </h2>
-      <p className="text-slate-500">
-        {format(hit.createdAt, "yyyy年MM月dd日")}
-      </p>
-      {user && <p className="truncate">{user.name}</p>}
+      {user && (
+        <div className="flex items-center">
+          <img
+            src={user?.avatarURL}
+            alt=""
+            className="w-10 h-10 block rounded-full mr-2"
+          />
+          <div>
+            <p className="truncate">{user.name}</p>
+            <p className="text-slate-500 text-sm">
+              {format(hit.createdAt, "yyyy年MM月dd日")}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
